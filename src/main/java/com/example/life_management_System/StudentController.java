c1d3783293890c34872ad550ebc1325c4a3ecb1a
Sangam <sangamchawale726@gmail.com>
2023-04-24 23:29:25 +0530

    Student management mini project is completed.

---
+++ src/main/java/com/example/life_management_System/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.life_management_System;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class StudentController {
+
+    HashMap<Integer, Student> hashMap = new HashMap<>();
+
+    @PostMapping("/registerStudent")
+    public void addStudent(@RequestBody Student student){
+
+        int key = student.getRollNo();
+
+        hashMap.put(key, student);
+
+    }
+
+    @GetMapping("/getStudentInfo")
+    public Student getStudent(@RequestParam("rollNo")Integer rollNo){
+
+        Student student = hashMap.get(rollNo);
+
+        return student;
+    }
+}
---
+++ src/test/java/com/example/life_management_System/LifeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.life_management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
